5c5958ee6746255866d0e68b3b2538def2aa2526
Wesley Jr <wesley_damazio@hotmail.com>
2023-07-06 21:00:57 -0300

    Update app5.js

--- app5.js
+++ app5.js
@@ -14,3 +14,30 @@ for(let valor1 = 1; valor1 <= 4; valor1++ ){
     }
     
 }
+
+console.log("Situação 3")
+
+ciclo: for(let valorA = 1; valorA<= 4; valorA++){
+
+    for(let valorB = 1; valorB <=3; valorB++){
+    console.log(`${valorA} x ${valorB} = ${valorA*valorB}`);
+
+    if (valorA*valorB == 9) break ciclo;
+
+    }
+}
+
+console.log("Situação 4")
+
+ciclo2: for(let valorX= 1; valorX<= 10; valorX++){
+
+    for(let valorY=1; valorY<= 10; valorY++){
+        console.log(`${valorX} x ${valorY} = ${valorX*valorY}`)
+
+        if(valorX * valorY == 10){
+            console.log("Interrompido");
+            break ciclo2;
+        }
+    }
+
+}
