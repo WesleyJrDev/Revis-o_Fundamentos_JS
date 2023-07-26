2648e6342541490af5a618bb4ffe86a311856840
Wesley Jr <wesley_damazio@hotmail.com>
2023-07-26 19:03:21 -0300

    Create formas_de_declarar_funcao.js

---
+++ formas_de_declarar_funcao.js
@@ -0,0 +1,18 @@
+console.clear();
+
+function dizer_ola1(){
+    console.log("Olá1");
+};
+
+// function expression, quando uma variavel é declarada com uma função.
+
+let dizer_ola2 = function(){
+    console.log("Olá2");
+};
+
+dizer_ola1()
+dizer_ola2()
+
+let nova_variavel = dizer_ola2;
+
+nova_variavel()
