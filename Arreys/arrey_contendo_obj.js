c8c569b7e31c748bb90922ede8fd0ced8b32ca6d
Wesley Jr <wesley_damazio@hotmail.com>
2023-08-17 19:21:34 -0300

    Create arrey_contendo_obj.js

---
+++ Arreys/arrey_contendo_obj.js
@@ -0,0 +1,8 @@
+let clientes = [
+    {nome: 'wesley', sobrenome: 'damazio', idade: 35},
+    {nome: 'leonice', sobrenome: 'damazio', idade: 51},
+    {nome: 'lara', sobrenome: 'damazio', idade: 13},
+    {nome: 'lonera', sobrenome: 'nogueira', idade: 15},
+];
+
+console.log(clientes[2].nome);
