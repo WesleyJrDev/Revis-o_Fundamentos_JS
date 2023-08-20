7fa8aab6db6a9a27589707fd0e6e9508bd2b75b6
Wesley Jr <wesley_damazio@hotmail.com>
2023-08-20 14:36:29 -0300

    Create metodos_do_arrey.js

---
+++ Arreys/metodos_do_arrey.js
@@ -0,0 +1,27 @@
+// POP remove o ultimo elemento
+
+let nomes = ['wesley','leonice', 'lara', 'lorena'];
+console.log(nomes)
+nomes.pop()
+console.log(nomes)
+
+let nomes_retirado = nomes.pop();
+console.log(nomes_retirado)
+
+// PUSH adiciona um elemento no final
+
+nomes.push('lore')
+console.log(nomes);
+
+// SHIFT remove o primeiro elemento
+
+nomes.shift()
+console.log(nomes)
+
+nome_retirado2 = nomes.shift()
+console.log(nome_retirado2)
+
+// UNSHIFT adiciona um elemento no inicio
+
+nomes.unshift('lara')
+console.log(nomes)
